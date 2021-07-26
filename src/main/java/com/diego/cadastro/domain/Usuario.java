4f27edc3bbd86467735da5b51aaca9d41e166d69
unknown <diego.reports@gmail.com>
2021-07-26 20:09:37 -0300

    implemetando repository

--- src/main/java/com/diego/cadastro/domain/Usuario.java
+++ src/main/java/com/diego/cadastro/domain/Usuario.java
@@ -1,8 +1,21 @@
 package com.diego.cadastro.domain;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Usuario {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
+
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String email;
